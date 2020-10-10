4d04bf412af9d66a8445294367c15434a1a8028d
RinHizakura <s921975628@gmail.com>
2020-10-10 16:21:21 +0800

    Delete redundant comment

--- main.c
+++ main.c
@@ -6,14 +6,13 @@
 int main()
 {
     static struct rs_control *rs_decoder;
-    rs_decoder = init_rs(12);
+    rs_decoder = init_rs(6);
 
     uint16_t par[10];
     uint8_t data8[] = {0x40, 0xd2, 0x75, 0x47, 0x76, 0x17, 0x32, 0x06,
                        0x27, 0x26, 0x96, 0xc6, 0xc6, 0x96, 0x70, 0xec};
     /* Initialize the parity buffer */
     memset(par, 0, sizeof(par));
-    /* Encode 512 byte in data8. Store parity in buffer par */
     encode_rs8(rs_decoder, data8, 16, par);
 
     for (int i = 0; i < 16; i++)
@@ -25,10 +24,7 @@ int main()
     data8[0] = 0x50;
     data8[1] = 0x50;
     data8[2] = 0x50;
-    data8[3] = 0x50;
-    data8[4] = 0x50;
 
-    /* Decode 512 byte in data8.*/
     int numerr = decode_rs8(rs_decoder, data8, 16, par);
     printf("numerr: %d\n", numerr);
 
